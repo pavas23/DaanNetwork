b3491ced4bc128f385544038242c749254ad0f1b
Dev Gala <114738592+devgala@users.noreply.github.com>
2024-02-24 19:01:12 +0530

    tp

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Outlet} from 'react-router-dom';
-import NgoDonationDrive from './components/Ngo/NgoDonatonDrive';
 
 function App() {
     return (
         <>
-            {/* <Outlet/> */}
-            <NgoDonationDrive/>
+            <Outlet/>
+            
         </>
     );
 }
