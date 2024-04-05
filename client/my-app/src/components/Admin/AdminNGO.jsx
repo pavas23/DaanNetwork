50e1545affbba8afc0b2826477cba44fee4d1275
Pritam <pritambasu14@gmail.com>
2024-04-06 02:42:22 +0530

    add verified/not verified badge to each ngo

--- client/my-app/src/components/Admin/AdminNGO.jsx
+++ client/my-app/src/components/Admin/AdminNGO.jsx
@@ -3,10 +3,12 @@ import styles from "../../css/Admin/AdminNGO.module.css";
 import { useEffect, useState } from "react";
 import swal from "sweetalert";
 import { useNavigate } from "react-router-dom";
+
 function AdminNGO() {
   const [ngos, setNgos] = useState([]);
   let navigate = useNavigate();
   const REACT_APP_APIURL = process.env.REACT_APP_APIURL;
+
   const getNGO = async () => {
     const resp = await fetch(`${REACT_APP_APIURL}/ngo/get-all-ngos`, {
       method: "GET",
@@ -19,9 +21,11 @@ function AdminNGO() {
       setNgos(json.ngo);
     }
   };
+
   useEffect(() => {
     getNGO();
   }, []);
+
   return (
     <div>
       <AdminNav />
@@ -37,38 +41,34 @@ function AdminNGO() {
                 " row mb-3 p-3 rounded align-items-center justify-content-between"
               }
             >
-             
-              <div
-                className="col-sm-10 d-flex row"
-              >
-                <span
-                  className={
-                    styles.person_name + " col-sm-4 d-flex align-items-center"
-                  }
-                >
+              <div className="col-sm-10 d-flex row">
+                <span className={styles.person_name + " col-sm-4 d-flex align-items-center"}>
                   {ngo.name}
                 </span>
-                <span
-                  className={
-                    styles.person_name + " col-sm-8 d-flex align-items-center"
-                  }
-                >
+                <span className={styles.person_name + " col-sm-8 d-flex align-items-center"}>
                   {ngo.address}
                 </span>
               </div>
               <div className="col-sm-1 d-flex justify-content-center">
                 <div
-                 onClick={() => {
-                  navigate("/admin-ngoInfo", {
-                    replace: false,
-                    state: { ngo: ngo },
-                  });
-                }}
+                  onClick={() => {
+                    navigate("/admin-ngoInfo", {
+                      replace: false,
+                      state: { ngo: ngo },
+                    });
+                  }}
                   className={"btn " + styles.view_btn}
                 >
                   View NGO
                 </div>
-              </div> 
+              </div>
+              <div className="col-sm-1">
+                {ngo.isVerified ? (
+                  <span className={styles.verified_badge}>Verified</span>
+                ) : (
+                  <span className={styles.not_verified_badge}>Not Verified</span>
+                )}
+              </div>
             </div>
           ))}
         </div>
