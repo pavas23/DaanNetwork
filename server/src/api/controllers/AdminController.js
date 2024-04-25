f6b7f39cbcc176198b4872265183be8012ed9e0b
SakshamBajaj18 <sakshambajaj18@gmail.com>
2024-04-25 23:46:34 +0530

    chartsdfg

--- server/src/api/controllers/AdminController.js
+++ server/src/api/controllers/AdminController.js
@@ -95,3 +95,136 @@ module.exports.adminLogin = async (req, res) => {
       .json({ status: false, desc: "Internal Server Error Occured" });
   }
 };
+
+const findState= (address)=>{
+  var state=address.split(",");
+  var temp=state[state.length-1];
+  temp=temp.trim();
+  return temp;
+}
+
+module.exports.getNgoStates= async (req, res) => {
+  try {
+    var ngo=await Ngo.find();
+    var states={};
+    ngo.forEach((ngo)=>{
+     var state=findState(ngo.address);
+      if(states[state]){
+        states[state]++;
+      }
+      else{
+        states[state]=1;
+      }
+    });
+    return res.status(200).json({ status: true, states: states });
+
+  }catch(err){
+    console.log(err);
+    return res
+      .status(500)
+      .json({ status: false, desc: "Internal Server Error Occured" });
+  }
+}
+
+module.exports.getDonorStates= async (req, res) => {
+  try {
+    var donor=await Donor.find();
+    var states={};
+    donor.forEach((donor)=>{
+     var state=donor.state;
+      if(states[state]){
+        states[state]++;
+      }
+      else{
+        states[state]=1;
+      }
+    });
+    return res.status(200).json({ status: true, states: states });
+
+  }catch(err){
+    console.log(err);
+    return res
+      .status(500)
+      .json({ status: false, desc: "Internal Server Error Occured" });
+  }
+}
+
+module.exports.getFoodDonationsStates= async (req, res) => {
+  try {
+    var donations=await FoodDonation.find();
+    var states={};
+    donations.forEach((donation)=>{
+     var state=findState(donation.pickUpLocation);
+      if(states[state]){
+        states[state]++;
+      }
+      else{
+        states[state]=1;
+      }
+    });
+    return res.status(200).json({ status: true, states: states });
+
+  }catch(err){
+    console.log(err);
+    return res
+      .status(500)
+      .json({ status: false, desc: "Internal Server Error Occured" });
+  }
+}
+
+module.exports.getBlockedUsersStates= async (req, res) => {
+  try {
+    var blockedUsers=await BlockedUsers.find();
+    var states={};
+    var ngos=[]
+
+    await Promise.all(blockedUsers.map(async (user) => {
+      const emailId = user.emailId;
+      const x = await Ngo.findOne({ emailId: emailId });
+      if (x !== null) {
+        ngos.push(x);
+      }
+    }));
+
+
+    ngos.forEach((ngo)=>{
+      var state=findState(ngo.address);
+      if(states[state]){
+        states[state]++;
+      }
+      else{
+        states[state]=1;
+      }
+    });
+    return res.status(200).json({ status: true, states: states });
+
+  }catch(err){
+    console.log(err);
+    return res
+      .status(500)
+      .json({ status: false, desc: "Internal Server Error Occured" });
+  }
+}
+
+module.exports.getVerifiedNgoStates= async (req, res) => {
+  try {
+    var ngo=await Ngo.find({isVerified:true});
+    var states={};
+    ngo.forEach((ngo)=>{
+     var state=findState(ngo.address);
+      if(states[state]){
+        states[state]++;
+      }
+      else{
+        states[state]=1;
+      }
+    });
+    return res.status(200).json({ status: true, states: states });
+
+  }catch(err){
+    console.log(err);
+    return res
+      .status(500)
+      .json({ status: false, desc: "Internal Server Error Occured" });
+  }
+}
